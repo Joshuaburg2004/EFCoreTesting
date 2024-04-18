a08e1b8fc049f9eeef8cb40c41de36ab17e5d668
Joshuaburg2004 <joshuaburg2004@hotmail.nl>
2024-04-18 12:21:42 +0200

    heb de onderdelen in apparte files gezet, ook installatieOnderdeel aangemaakt

---
+++ Data/InstallatieOnderdeel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class InstallatieOnderdeel
+{
+    [Required]
+    [StringLength(100, ErrorMessage = "Serie nummer kan niet langer dan 100 characters zijn.")]
+    [Key]
+    public string? SerieNummer { get; set; }
+    [Required]
+    [StringLength(100, ErrorMessage = "De naam van de buitendienst medewerker kan niet langer dan 100 characters zijn.")]
+    public string? WerkerNaam { get; set; }
+    [Required]
+    public string? Adres { get; set; }
+    [Required]
+    public int Hoeveelheid { get; set; }
+
+    [Required]
+    [StringLength(50, ErrorMessage = "Het soort onderdeel kan niet langer zijn dan 50 characters.")]
+    public string? Soort { get; set; }
+
+    public string? Comment { get; set; }
+}
--- Data/OnderdeelContext.cs
+++ Data/OnderdeelContext.cs
@@ -7,6 +7,7 @@ public class OnderdeelContext : DbContext
 {
     public DbSet<Onderdeel> Onderdelen { get; set; }
     public DbSet<OnderwegOnderdeel> OnderwegOnderdelen { get; set; }
+    public DbSet<InstallatieOnderdeel> InstallatieOnderdelen { get; set; }
     public string DbPath { get; }
     public OnderdeelContext()
     {
@@ -23,45 +24,3 @@ public class OnderdeelContext : DbContext
     }
 }
 
-public class OnderwegOnderdeel
-{
-    [Required]
-    [StringLength(100, ErrorMessage = "Serie nummer kan niet langer dan 100 characters zijn.")]
-    [Key]
-    public string? SerieNummer { get; set; }
-
-    [StringLength(100, ErrorMessage = "De naam van de buitendienst medewerker kan niet langer dan 100 characters zijn.")]
-    public string? WerkerNaam { get; set; }
-
-    [Required]
-    public int Hoeveelheid { get; set; }
-
-    [Required]
-    [StringLength(50, ErrorMessage = "Het soort onderdeel kan niet langer zijn dan 50 characters.")]
-    public string? Soort { get; set; }
-
-    public string? Comment { get; set; }
-}
-public class Onderdeel
-{
-
-    [Required]
-    [StringLength(100, ErrorMessage = "Serie nummer kan niet langer dan 100 characters zijn.")]
-    [Key]
-    public string? SerieNummer { get; set; }
-
-    [StringLength(100, ErrorMessage = "De naam van het onderdeel kan niet langer dan 100 characters zijn.")]
-    public string? Naam { get; set; }
-
-    [StringLength(25, ErrorMessage = "Locatie naam kan niet langer dan 25 characters zijn.")]
-    public string? Locatie { get; set; }
-
-    [Required]
-    public int Hoeveelheid { get; set; }
-
-    [Required]
-    [StringLength(50, ErrorMessage = "Het soort onderdeel kan niet langer zijn dan 50 characters.")]
-    public string? Soort { get; set; }
-
-    public string? Comment { get; set; }
-}
